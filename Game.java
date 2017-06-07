8b18f52e088ced732a0465d25896b4ba2c13aa81
Byron <byronblocker@gmail.com>
2017-06-06 21:21:57 -0700

    commented out some fragmented thoughts

--- Game.java
+++ Game.java
@@ -2,15 +2,14 @@ public class Game {
   Player player1 = new Player();
   Dice die1 = new Dice();
 
-  // while(gameRunning){
-  //     playTurn(player1);
-  //     playTurn(player2);
-  // }
-
   public void playTurn(){
     int result1 = die1.roll();
     player1.score += result1;
-    System.out.println(String.format("You rolled a %d and your score is now %d", result1, player1.score));
+    // if (result1 != 1){
+    //   System.out.println(String.format("You rolled a %d and your score is now %d", result1, player1.score));
+    // } else {
+    //   System.out.println(String.format("You rolled a %d, your score is"));
+    // }
   }
 
 
